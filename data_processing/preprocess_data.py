fc02baff60de8ba4983ad209823a7db4b96e825e
Marine <marine@intelinair.com>
2023-02-21 14:44:47 +0400

    Refactored Upper-Case and Column Names

--- data_processing/preprocess_data.py
+++ data_processing/preprocess_data.py
@@ -28,11 +28,11 @@ def save_tfrecord(data, tfrecord_path):
             example = tf.train.Example(
                 features=tf.train.Features(
                     feature={
-                        "ACC_x": tf.train.Feature(float_list=tf.train.FloatList(value=[row.ACC_x])),
-                        "ACC_y": tf.train.Feature(float_list=tf.train.FloatList(value=[row.ACC_y])),
-                        "ACC_z": tf.train.Feature(float_list=tf.train.FloatList(value=[row.ACC_z])),
-                        "PPG": tf.train.Feature(float_list=tf.train.FloatList(value=[row.PPG])),
-                        "HR": tf.train.Feature(float_list=tf.train.FloatList(value=[row.Label])),
+                        "acc_x": tf.train.Feature(float_list=tf.train.FloatList(value=[row.acc_x])),
+                        "acc_y": tf.train.Feature(float_list=tf.train.FloatList(value=[row.acc_y])),
+                        "acc_z": tf.train.Feature(float_list=tf.train.FloatList(value=[row.acc_z])),
+                        "ppg": tf.train.Feature(float_list=tf.train.FloatList(value=[row.ppg])),
+                        "heart_rate": tf.train.Feature(float_list=tf.train.FloatList(value=[row.label])),
                     }
                 )
             )
@@ -46,54 +46,45 @@ def preprocess_user_data(user_no):
         data = pickle.load(f, encoding="latin-1")
 
     # TODO figure out activity sampling rate
-    activity = pd.DataFrame(data["activity"]).astype(int)
-    activity.columns = ["Activity"]
-
-    label = pd.DataFrame(data["label"])
-    label.columns = ["Label"]
+    activity = pd.DataFrame(data["activity"], columns=["activity"]).astype(int)
+    label = pd.DataFrame(data["label"], columns=["label"])
 
     signal = pd.DataFrame(data["signal"])
-
-    ACC = pd.DataFrame(signal["wrist"].ACC)
-    ACC.columns = ["ACC_x", "ACC_y", "ACC_z"]
-    ACC.reset_index(drop=True, inplace=True)
-
-    PPG = pd.DataFrame(signal["wrist"].BVP)
-    PPG.columns = ["PPG"]
-    PPG.reset_index(drop=True, inplace=True)
+    acc_df = pd.DataFrame(signal["wrist"].ACC, columns=["acc_x", "acc_y", "acc_z"]).reset_index(drop=True)
+    ppg_df = pd.DataFrame(signal["wrist"].BVP, columns=["ppg"]).reset_index(drop=True)
 
     # Add timestamp to the data
-    ACC["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(ACC), freq=f"{1 / ACC_SR}S")
-    PPG["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(PPG), freq=f"{1 / BVP_SR}S")
+    acc_df["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(acc_df), freq=f"{1 / ACC_SR}S")
+    ppg_df["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(ppg_df), freq=f"{1 / BVP_SR}S")
     label["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(label), freq=f"{1 / HR_SR}S")
 
     # Set this column as index
-    ACC.set_index("timestamp", inplace=True)
-    PPG.set_index("timestamp", inplace=True)
+    acc_df.set_index("timestamp", inplace=True)
+    ppg_df.set_index("timestamp", inplace=True)
     label.set_index("timestamp", inplace=True)
 
     # Resample the data
-    ACC = resample(ACC, SECOND_SR)
-    PPG = resample(PPG, SECOND_SR)
-    HR = resample(label, SECOND_SR)
+    acc_df = resample(acc_df, SECOND_SR)
+    ppg_df = resample(ppg_df, SECOND_SR)
+    hr_df = resample(label, SECOND_SR)
 
-    logger.info(f"Length of ACC: {len(ACC)}")
-    logger.info(f"Length of PPG: {len(PPG)}")
-    logger.info(f"Length of HR: {len(HR)}")
+    logger.info(f"Length of ACC: {len(acc_df)}")
+    logger.info(f"Length of PPG: {len(ppg_df)}")
+    logger.info(f"Length of HR: {len(hr_df)}")
 
     # Take data points only with these timestamps
-    min_sample_time = min(ACC.index[-1], PPG.index[-1], HR.index[-1])
+    min_sample_time = min(acc_df.index[-1], ppg_df.index[-1], hr_df.index[-1])
     timestamps = pd.timedelta_range(start="00:00:00", end=min_sample_time, freq=f"{1 / NEW_SR}S")
-    ACC = ACC.loc[timestamps]
-    PPG = PPG.loc[timestamps]
-    HR = HR.loc[timestamps]
+    acc_df = acc_df.loc[timestamps]
+    ppg_df = ppg_df.loc[timestamps]
+    hr_df = hr_df.loc[timestamps]
 
-    logger.info(f"Length of ACC: {len(ACC)}")
-    logger.info(f"Length of PPG: {len(PPG)}")
-    logger.info(f"Length of HR: {len(HR)}")
+    logger.info(f"Length of ACC: {len(acc_df)}")
+    logger.info(f"Length of PPG: {len(ppg_df)}")
+    logger.info(f"Length of HR: {len(hr_df)}")
 
     # join the data
-    data = pd.concat([ACC, PPG, HR], axis=1)
+    data = pd.concat([acc_df, ppg_df, hr_df], axis=1)
     data.head()
 
     # Save the data
