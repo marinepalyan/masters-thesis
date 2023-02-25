d3ad80369d3723ed4d0cc589b52dd61ee3bbbe1d
Marine <marine@intelinair.com>
2023-02-25 18:15:27 +0400

    Added Activity Column and Fixed Docstring

--- data_processing/preprocess_data.py
+++ data_processing/preprocess_data.py
@@ -5,40 +5,36 @@ import pickle
 
 import pandas as pd
 import tensorflow as tf
-from pandas_tfrecords import pd2tf
 
 logger = logging.getLogger(__name__)
 
 BVP_SR = 64
 ACC_SR = 32
 HR_SR = 0.5
-SECOND_SR = 1000
-NEW_SR = 25  # Use 25Hz as the sampling rate
+ACTIVITY_SR = 4
+RESAMPLING_SR = 25  # Use 25Hz as the sampling rate
 TOTAL_NUM_OF_USERS = 15
 
 
-# TODO sliding window approach?
+def resample(df: pd.DataFrame, new_sr: int, method: str = "linear") -> pd.DataFrame:
+    """Resample and linearly interpolate the data to the new sampling rate.
 
-
-def resample(df: pd.DataFrame, new_sr: int) -> pd.DataFrame:
-    """
-    Resample and linearly interpolate the data to the new sampling rate.
     Args:
         df: dataframe with timestamp as index
         new_sr: new sampling rate
+        method: interpolation method
+
     Returns:
         resampled dataframe
     """
-    # The resampling returns groups of 1 row, so we need to take the mean of the values
-    # to bring it to DataFrame format again
-    df = df.resample(f"{1 / new_sr}S").mean()
-    df = df.interpolate(method="linear")
+    # Resample
+    df = df.resample(f"{1 / new_sr}S").interpolate(method=method).dropna()
     return df
 
 
 def save_tfrecord(data: pd.DataFrame, tfrecord_path: str) -> None:
-    """
-    Save the data as TFRecord
+    """Save the data as TFRecord.
+
     Args:
         data: dataframe with timestamp as index
         tfrecord_path: save path
@@ -60,8 +56,8 @@ def save_tfrecord(data: pd.DataFrame, tfrecord_path: str) -> None:
 
 
 def save_output(data: pd.DataFrame, output_dir: str) -> None:
-    """
-    Save the data as CSV and TFRecord
+    """Save the data as CSV and TFRecord.
+
     Args:
         data: dataframe with timestamp as index
         output_dir: save path
@@ -78,8 +74,8 @@ def save_output(data: pd.DataFrame, output_dir: str) -> None:
 
 
 def preprocess_user_data(user_no: int, output_dir: str) -> None:
-    """
-    Preprocess the data for a single user and save results in the output directory.
+    """Preprocess the data for a single user and save results in the output directory.
+
     Args:
         user_no: user index
         output_dir: output directory
@@ -89,7 +85,6 @@ def preprocess_user_data(user_no: int, output_dir: str) -> None:
     with open(path, "rb") as f:
         data = pickle.load(f, encoding="latin-1")
 
-    # TODO figure out activity sampling rate
     activity = pd.DataFrame(data["activity"], columns=["activity"]).astype(int)
     label = pd.DataFrame(data["label"], columns=["label"])
 
@@ -101,34 +96,30 @@ def preprocess_user_data(user_no: int, output_dir: str) -> None:
     acc_df["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(acc_df), freq=f"{1 / ACC_SR}S")
     ppg_df["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(ppg_df), freq=f"{1 / BVP_SR}S")
     label["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(label), freq=f"{1 / HR_SR}S")
+    activity["timestamp"] = pd.timedelta_range(start="00:00:00", periods=len(activity), freq=f"{1 / ACTIVITY_SR}S")
 
     # Set this column as index
     acc_df.set_index("timestamp", inplace=True)
     ppg_df.set_index("timestamp", inplace=True)
     label.set_index("timestamp", inplace=True)
+    activity.set_index("timestamp", inplace=True)
 
     # Resample the data
-    acc_df = resample(acc_df, SECOND_SR)
-    ppg_df = resample(ppg_df, SECOND_SR)
-    hr_df = resample(label, SECOND_SR)
-
-    logger.info(f"Length of ACC: {len(acc_df)}")
-    logger.info(f"Length of PPG: {len(ppg_df)}")
-    logger.info(f"Length of HR: {len(hr_df)}")
+    acc_df = resample(acc_df, RESAMPLING_SR)
+    ppg_df = resample(ppg_df, RESAMPLING_SR)
+    hr_df = resample(label, RESAMPLING_SR)
+    activity_df = resample(activity, RESAMPLING_SR, method="nearest").astype(int)
 
     # Take data points only with these timestamps
-    min_sample_time = min(acc_df.index[-1], ppg_df.index[-1], hr_df.index[-1])
-    timestamps = pd.timedelta_range(start="00:00:00", end=min_sample_time, freq=f"{1 / NEW_SR}S")
-    acc_df = acc_df.loc[timestamps]
-    ppg_df = ppg_df.loc[timestamps]
-    hr_df = hr_df.loc[timestamps]
-
-    logger.info(f"Length of ACC: {len(acc_df)}")
-    logger.info(f"Length of PPG: {len(ppg_df)}")
-    logger.info(f"Length of HR: {len(hr_df)}")
+    min_sample_time = min(acc_df.index[-1], ppg_df.index[-1], hr_df.index[-1], activity_df.index[-1])
+    acc_df = acc_df[:min_sample_time]
+    ppg_df = ppg_df[:min_sample_time]
+    hr_df = hr_df[:min_sample_time]
+    activity_df = activity_df[:min_sample_time]
+    assert len(acc_df) == len(ppg_df) == len(hr_df) == len(activity_df)
 
     # join the data
-    data = pd.concat([acc_df, ppg_df, hr_df], axis=1)
+    data = pd.concat([acc_df, ppg_df, hr_df, activity_df], axis=1)
     save_output(data, output_dir)
 
 
