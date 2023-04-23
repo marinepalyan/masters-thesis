efdb03ea9f03967e86f72451449db061eee2acfc
Marine <marine@intelinair.com>
2023-04-24 00:00:52 +0400

    Fixed Dense Model

--- models/dense.py
+++ models/dense.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 
 def get_dense_model(input_shape: Tuple[int, int]) -> tf.keras.Model:
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(32, activation='relu', input_shape=input_shape),
+        tf.keras.layers.Input(shape=input_shape),
+        tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(1)
     ])
     return model
